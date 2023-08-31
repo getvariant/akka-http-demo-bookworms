258d0e18d26d3be47aa53db8d2603fe82117327a
Igor Urisman <igor@Igors-MacBook-Pro.local>
2023-08-31 15:34:50 -0700

    WIP

--- variant/src/main/java/com/variant/demo/bookworms/spi/UserTargetingHook.java
+++ variant/src/main/java/com/variant/demo/bookworms/spi/UserTargetingHook.java
@@ -12,17 +12,23 @@ import java.util.Optional;
 /**
  * Variant Code Variation Server.
  * Bookworms demo application.
- * Targeting hook targets user for the Suggestions A/B test
- *   'WithSuggestions' -> WithSuggestions experience
- *   any other user    -> deffer to the default hook
  */
 
 public class UserTargetingHook implements VariationTargetingLifecycleHook {
 	@Override
 	public Optional<Variation.Experience> post(VariationTargetingLifecycleEvent event) {
-		if ("WithSuggestions".equals(event.getSession().getAttributes().get("user")))
-			return event.getVariation().getExperience("WithSuggestions");
-		else
-			return Optional.empty();
+		final String user = event.getSession().getAttributes().get("user");
+		final Variation variation = event.getVariation();
+		return switch (variation.getName()) {
+			case "Suggestions" -> {
+				yield "WithSuggestions".equals(user) ?
+					variation.getExperience("WithSuggestions") : Optional.empty();
+			}
+			case "ReputationFF" -> {
+				yield "WithReputation".equals(user) ?
+					variation.getExperience("WithReputation") : variation.getExperience("NoReputation");
+			}
+			default -> throw new RuntimeException(String.format("Unknown variation %s", variation));
+		};
 	}
 }
