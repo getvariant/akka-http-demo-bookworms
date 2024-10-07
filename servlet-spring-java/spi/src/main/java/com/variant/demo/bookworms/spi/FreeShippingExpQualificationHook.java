0abdb000dd5e6fc80ddbbe57685551ca40705daa
Igor Urisman <igor@Igors-MacBook-Pro.local>
2024-10-07 14:12:58 -0700

    Move petclinic into own directory /webapp

---
+++ servlet-spring-java/spi/src/main/java/com/variant/demo/bookworms/spi/FreeShippingExpQualificationHook.java
@@ -0,0 +1,24 @@
+package com.variant.demo.bookworms.spi;
+
+import com.variant.server.spi.TraceEvent;
+import com.variant.server.spi.QualificationLifecycleEvent;
+import com.variant.server.spi.QualificationLifecycleHook;
+
+import java.util.Optional;
+
+/**
+ * Variant CVM.
+ * Bookworms demo application.
+ */
+
+public class FreeShippingExpQualificationHook implements QualificationLifecycleHook {
+	@Override
+	public Optional<Boolean> post(QualificationLifecycleEvent event) {
+		var qualified = Boolean.valueOf(event.getSession().getAttributes().get("isInactive"));
+		var traceMessage = String.format(
+			"%s: %s user '%s'",
+			getClass().getSimpleName(), qualified ? "Qualified" : "Disqualified", event.getSession().getOwnerId().orElse("?"));
+		event.getSession().triggerEvent(TraceEvent.of(traceMessage));
+		return Optional.of(qualified);
+	}
+}
